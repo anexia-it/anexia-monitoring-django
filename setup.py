51ab213cc79b094ea683bffe692660ad435db1b6
Harald Nezbeda <hn@anexia.at>
2017-06-01 11:35:11 +0200

    Adds PyPI package data

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='anexia-monitor',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Finds packages that require updates on a python environment.',
+    long_description=README,
+    url='http://www.anexia-it.com/',
+    author='Anexia',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=[
+        'updatable',
+    ],
+)
